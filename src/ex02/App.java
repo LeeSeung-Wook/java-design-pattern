d0da76b484ba88ada025590ea52f572bfca9019e
LeeSeungWook <tmxlqms135@naver.com>
2026-02-10 17:38:27 +0900

    템플릿 메서드 패턴 완료

--- src/ex02/App.java
+++ src/ex02/App.java
@@ -1,14 +1,11 @@
 package ex02;
 
-// SOLID : 객체지향원칙 (D, O)
-// DIP : 추상적인 것에 의존하라.
-// OCP : 새로운 코드에는 OPEN, 기존 코드에는 CLOSE
+// 프록시 패턴
 public class App {
     public static void main(String[] args) {
         Tiger tiger = new Tiger();
         Mouse mouse = new Mouse();
-        Doorman doorman = new Doorman();
-        doorman.쫓아내(tiger);
+        Doorman doorman = new DoormanProxy();
         doorman.쫓아내(mouse);
     }
 }
---
+++ src/ex05/teacher/PythonTeacher.java
@@ -0,0 +1,8 @@
+package ex05.teacher;
+
+public class PythonTeacher extends Teacher{
+    @Override
+    protected void 강의하기() {
+        System.out.println("Python 강의하기");
+    }
+}
